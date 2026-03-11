89c879225e1d6ee399a68608907e741df8b98aa1
JerlineFringsha <jerlinfringsha12@gmail.com>
2026-03-11 11:38:48 +0530

    Set Styles, Comp, Assets, Constants

---
+++ mobile/app/index.tsx
@@ -0,0 +1,24 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import { Image } from "expo-image";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/modal">Open Modal</Link>
+      <Image
+        source={{
+          uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+    </View>
+  );
+}
---
+++ mobile/app/modal.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from "react-native";
+
+
+export default function modal() {
+  return (
+    <View>
+        <Text>This is a modal!</Text>
+      
+    </View>
+  )
+}
